2e2259e13a565d575d3ed87daf1c7a563b9c2f82
Nikita <hekitko0@gmail.com>
2019-09-21 20:12:29 +0300

    Blog.Core.DataAccess add DBContext and initializer

---
+++ Blog.DataAccess/BlogContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Blog.Core.Models;
+
+namespace Blog.DataAccess
+{
+    /// <summary>
+    /// Context for our Project
+    /// </summary>
+    public class BlogContext : DbContext
+    {
+        public BlogContext() : base("BlogContext")
+        {
+            Database.SetInitializer(new BlogInitializer());
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Feedback> FeedbackItems { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Question>()
+                .HasRequired<Profile>(s => s.Profile)
+                .WithMany(s => s.Questions)
+                .HasForeignKey(s => s.ProfileId);
+            modelBuilder.Entity<Variant>()
+                .HasRequired<Question>(s => s.Question)
+                .WithMany(s => s.Variants)
+                .HasForeignKey(s => s.QuestionId);
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            
+        }
+    }
+}
---
+++ Blog.DataAccess/BlogInitializer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using Blog.Core.Models;
+
+namespace Blog.DataAccess
+{
+    /// <summary>
+    /// Initializer block for our Blog
+    /// </summary>
+    public class BlogInitializer : System.Data.Entity.DropCreateDatabaseAlways<BlogContext>
+    {
+        protected override void Seed(BlogContext context)
+        {
+            // create
+            var articles = new List<Article>
+            {
+                new Article
+                {
+                    Name = "Article 1", Date = DateTime.Parse("2019-09-19"),
+                    Text =
+                        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem voluptate aspernatur incidunt maxime beatae deleniti exercitationem repellat, adipisci tempora repellendus doloremque ratione, laboriosam hic dolores ipsa asperiores illo reiciendis similique ut, eos itaque quasi tempore excepturi? Magnam esse voluptatibus dolor."
+                },
+                new Article
+                {
+                    Name = "Article 2", Date = DateTime.Parse("2019-09-19"),
+                    Text =
+                        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem voluptate aspernatur incidunt maxime beatae deleniti exercitationem repellat, adipisci tempora repellendus doloremque ratione, laboriosam hic dolores ipsa asperiores illo reiciendis similique ut, eos itaque quasi tempore excepturi? Magnam esse voluptatibus dolor."
+                },
+                new Article
+                {
+                    Name = "Article 3", Date = DateTime.Parse("2019-09-19"),
+                    Text =
+                        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem voluptate aspernatur incidunt maxime beatae deleniti exercitationem repellat, adipisci tempora repellendus doloremque ratione, laboriosam hic dolores ipsa asperiores illo reiciendis similique ut, eos itaque quasi tempore excepturi? Magnam esse voluptatibus dolor."
+                },
+                new Article
+                {
+                    Name = "Article 4", Date = DateTime.Parse("2019-09-19"),
+                    Text =
+                        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem voluptate aspernatur incidunt maxime beatae deleniti exercitationem repellat, adipisci tempora repellendus doloremque ratione, laboriosam hic dolores ipsa asperiores illo reiciendis similique ut, eos itaque quasi tempore excepturi? Magnam esse voluptatibus dolor."
+                }
+            };
+            // and add entity
+            articles.ForEach(s => context.Articles.Add(s));
+            context.SaveChanges();
+            var feedbackItems = new List<Feedback>
+            {
+                new Feedback
+                {
+                    Author = "Author 1", Date = DateTime.Parse("2019-09-19"),
+                    Text =
+                        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit in animi, quisquam vel non placeat minus numquam accusamus voluptatibus atque."
+                },
+                new Feedback
+                {
+                    Author = "Author 2", Date = DateTime.Parse("2019-09-19"),
+                    Text =
+                        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit in animi, quisquam vel non placeat minus numquam accusamus voluptatibus atque."
+                },
+                new Feedback
+                {
+                    Author = "Author 3", Date = DateTime.Parse("2019-09-19"),
+                    Text =
+                        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit in animi, quisquam vel non placeat minus numquam accusamus voluptatibus atque."
+                },
+                new Feedback
+                {
+                    Author = "Author 4", Date = DateTime.Parse("2019-09-19"),
+                    Text =
+                        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit in animi, quisquam vel non placeat minus numquam accusamus voluptatibus atque."
+                },
+            };
+            feedbackItems.ForEach(s => context.FeedbackItems.Add(s));
+            context.SaveChanges();
+
+
+
+            Profile profile = new Profile
+            {
+                ProfilePreview = "На сколько вы человек?",
+
+                Results = new Dictionary<int, string>()
+                {
+                    {1,"Ты человек, но не понятно мужчина или женщина."},
+                    {2, "Ты мужчинка, но не человек." },
+                    {3,"Ты человеко-мужчина, всё решено." },
+                    {4, "Ты человеко-женщина." },
+                    {5,"Ты мужчино-женщина, но не человек(оно и понятно)." },
+                    {6,"Ты не умеешь отвечать в анкетировании." }
+                },
+                Questions = new List<Question>
+                {
+                    new Question
+                    {
+                        Text = "Ты человек?",
+                        Variants = new List<Variant>
+                        {
+                            new Variant{Id = 1,Text = "Да", Value = false},
+                            new Variant{Id = 2,Text = "Нет", Value = false},
+                            new Variant{Id=3,Text = "Не знаю", Value = false}
+                        }
+                    },
+                    new Question
+                    {
+                        Text = "Ты мужчина?",
+                        Variants = new List<Variant>
+                        {
+                            new Variant{Id = 1,Text = "Да", Value = false},
+                            new Variant{Id = 2,Text = "Нет", Value = false},
+                            new Variant{Id=3,Text = "Не знаю", Value = false}
+                        }
+                    },
+                    new Question
+                    {
+                        Text = "Ты женщина?",
+                        Variants = new List<Variant>
+                        {
+                            new Variant{Id = 1,Text = "Да", Value = false},
+                            new Variant{Id = 2,Text = "Нет", Value = false},
+                            new Variant{Id=3,Text = "Не знаю", Value = false}
+                        }
+                    }
+                }
+
+            };
+
+            context.Profiles.Add(profile);
+            context.SaveChanges();
+        }
+    }
+}
