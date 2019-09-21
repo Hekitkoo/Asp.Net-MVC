4c55c7b974c37adeee91ad639f09e6f70eae1f03
Nikita <hekitko0@gmail.com>
2019-09-21 19:56:11 +0300

    Blog.UI add Helpers

---
+++ Blog.UI/Helpers/ViewHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Blog
+{
+    public static class ViewHelper
+    {
+        /// <summary>
+        /// Unsorted list helper
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public static MvcHtmlString UnOrderedList(this HtmlHelper helper, string item)
+        {
+            TagBuilder tagUl = new TagBuilder("ul");
+
+            TagBuilder tagLi = new TagBuilder("li")
+                {
+                    InnerHtml = item
+                };
+                tagUl.InnerHtml += tagLi;
+            return new MvcHtmlString(tagUl.ToString());
+        }
+    }
+}
