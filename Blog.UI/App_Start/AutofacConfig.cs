f4e674ef1ba5cb6aa1c745ed85d894ac9f051a24
Nikita <hekitko0@gmail.com>
2019-09-21 19:55:27 +0300

    Blog.UI add DI

---
+++ Blog.UI/App_Start/AutofacConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using Autofac;
+using Blog;
+using Autofac.Integration.Mvc;
+using Blog.Core.Interfaces;
+using Blog.Core.Models;
+using Blog.DataAccess;
+using Blog.Services;
+
+namespace Blog.UI.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            // Get instance of autofac
+            var builder = new ContainerBuilder();
+            // registe our controllers, using scan
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // register our context
+            builder.RegisterType<BlogContext>().AsSelf();
+            builder.RegisterType<ArticleServices>().As<IArticleServices>();
+            builder.RegisterType<GuestServices>().As<IGuestServices>();
+            builder.RegisterType<ProfileServices>().As<IProfileServices>();
+            // build our container
+            var container = builder.Build();
+
+            // and resolve
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
--- Blog.UI/App_Start/RouteConfig.cs
+++ Blog.UI/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Blog.UI
+namespace Blog
 {
     public class RouteConfig
     {
@@ -16,7 +16,7 @@ namespace Blog.UI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Article", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
